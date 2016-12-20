49deb3aeecadb02518e4d0d21d7757056e2292be
Fokke Zandbergen <mail@fokkezb.nl>
2016-12-20 15:05:05 +0100

    Make device event's field optional, closes #39

--- test/client.js
+++ test/client.js
@@ -1,22 +1,26 @@
+var sinon = require('sinon');
 var should = require('should');
+require('should-sinon');
 
 var ttn = require('..');
 
 var BROKER = 'iot.eclipse.org';
+var BYPASS = true;
+var WAIT_FOR = BYPASS ? 100 : 1000;
 
-describe('Client', function() {
+describe('Client', function () {
 
-  describe('#new', function() {
-    it('should create client', function() {
+  describe('#new', function () {
+    it('should create client', function () {
       var client = createClient();
       client.should.be.an.instanceOf(ttn.Client);
     });
   });
 
-  describe('#on(connect)', function() {
-    it('shoudl emit event', function(done) {
+  describe('#on(connect)', function () {
+    it('shoudl emit event', function (done) {
       var client = createClient();
-      client.on('connect', function(connack) {
+      client.on('connect', function (connack) {
         should(connack).be.an.Object();
         client.end();
         done();
@@ -24,29 +28,22 @@ describe('Client', function() {
     });
   });
 
-  describe('#on(activation)', function() {
-    it('should error on devId with separator or wildcard', function() {
+  describe('#on(activation)', function () {
+    it('should error on devId with separator or wildcard', function () {
       var client = createClient();
-      (function() {
+      (function () {
         client.on('activation', 'my/uno');
       }).should.throw();
-      (function() {
+      (function () {
         client.on('activation', '#');
       }).should.throw();
-      (function() {
+      (function () {
         client.on('activation', 'my/+');
       }).should.throw();
     });
-    it('should emit event', function(done) {
+    it('should emit event', function (done) {
       var client = createClient();
-      client.on('activation', function(deviceId, data) {
-        should(deviceId).be.a.String().and.equal('a-device');
-        should(data).be.an.Object();
-        should(data.dev_addr).be.a.String().and.equal('26012723');
-        client.end();
-        done();
-      });
-      client.mqtt.publish(client.appId + '/devices/a-device/events/activations', JSON.stringify({
+      var data = {
         "app_eui": "70B3D57EF000001C",
         "dev_eui": "0004A30B001B7AD2",
         "dev_addr": "26012723",
@@ -66,79 +63,84 @@ describe('Client', function() {
             "rf_chain": 1
           }]
         }
-      }));
+      };
+      var callback = sinon.spy();
+      client.on('activation', callback);
+      client.mqtt.publish(client.appId + '/devices/a-device/events/activations', JSON.stringify(data));
+      setTimeout(function () {
+        callback.should.be.calledOnce().calledWith('a-device', data);
+        done();
+      }, WAIT_FOR);
     });
   });
 
-  describe('#on(device)', function() {
-    it('should error on field wildcard', function() {
+  describe('#on(device)', function () {
+    it('should error on field wildcard', function () {
       var client = createClient();
-      (function() {
+      (function () {
         client.on('device', null, '#');
       }).should.throw();
-      (function() {
+      (function () {
         client.on('device', null, 'down/+');
       }).should.throw();
     });
-    it('should emit event', function(done) {
+    it('should emit event', function (done) {
       var client = createClient();
-      client.on('device', null, 'activations', function(deviceId, data) {
-        should(deviceId).be.a.String().and.equal('a-device');
-        should(data).be.an.Object();
-        should(data.dev_addr).be.a.String().and.equal('26012723');
-        client.end();
-        done();
-      });
-      client.mqtt.publish(client.appId + '/devices/a-device/events/activations', JSON.stringify({
+      var data = {
         "dev_addr": "26012723"
-      }));
-    });
-    it('should emit event for specific device', function(done) {
-      var client = createClient();
-      client.on('device', 'a-device', 'activations', function(deviceId, data) {
-        should(deviceId).be.a.String().and.equal('a-device');
-        should(data).be.an.Object();
-        should(data.dev_addr).be.a.String().and.equal('26012723');
-        client.end();
+      };
+      var callback = sinon.spy();
+      client.on('device', null, 'activations', callback);
+      client.mqtt.publish(client.appId + '/devices/a-device/events/activations', JSON.stringify(data));
+      setTimeout(function () {
+        callback.should.be.calledOnce().calledWith('a-device', data);
         done();
-      });
-      client.mqtt.publish(client.appId + '/devices/a-device/events/activations', JSON.stringify({
+      }, WAIT_FOR);
+    });
+    it('should emit event (for specific device and/or event)', function (done) {
+      var client = createClient();
+      var data = {
         "dev_addr": "26012723"
-      }));
-    });
-    it('should emit event with deep field and payload_raw', function(done) {
-      var client = createClient();
-      client.on('device', 'a-device', 'down/scheduled', function(deviceId, data) {
-        should(deviceId).be.a.String().and.equal('a-device');
-        should(data).be.an.Object();
-        should(data.payload_raw).be.an.instanceOf(Buffer).with.lengthOf(1);
-        should(data.payload_raw[0]).equal(1);
-        client.end();
+      };
+      var callback = sinon.spy();
+      var callbackD = sinon.spy();
+      var callbackDE = sinon.spy();
+      var callbackE = sinon.spy();
+      client.on('device', callback);
+      client.on('device', 'a-device', callbackD);
+      client.on('device', 'a-device', 'activations', callbackDE);
+      client.on('device', null, 'activations', callbackE);
+      client.mqtt.publish(client.appId + '/devices/a-device/events/activations', JSON.stringify(data));
+      setTimeout(function () {
+        callback.should.be.calledOnce().calledWith('a-device', data);
+        callbackD.should.be.calledOnce().calledWith('a-device', data);
+        callbackDE.should.be.calledOnce().calledWith('a-device', data);
+        callbackE.should.be.calledOnce().calledWith('a-device', data);
         done();
-      });
-      client.mqtt.publish(client.appId + '/devices/a-device/events/down/scheduled', JSON.stringify({
+      }, WAIT_FOR);
+    });
+    it('should emit event with deep field and payload_raw', function (done) {
+      var client = createClient();
+      var data = {
         "port": 1,
         "payload_raw": "AQ=="
-      }));
+      };
+      var callback = sinon.spy();
+      client.on('device', 'a-device', 'down/scheduled', callback);
+      client.mqtt.publish(client.appId + '/devices/a-device/events/down/scheduled', JSON.stringify(data));
+      setTimeout(function () {
+        data.payload_raw = new Buffer(data.payload_raw, 'base64');
+        callback.should.be.calledOnce().calledWith('a-device', data);
+        done();
+      }, WAIT_FOR);
     });
   });
 
-  describe('#on(message)', function() {
+  describe('#on(message)', function () {
 
-    it('should emit event', function(done) {
+    it('should emit event', function (done) {
       var client = createClient();
-      client.on('message', function(deviceId, data) {
-        should(deviceId).be.a.String().and.equal('a-device');
-        should(data).be.an.Object();
-        should(data.payload_fields).be.an.Object();
-        should(data.payload_fields.led).be.true();
-        should(data.payload_raw).be.an.instanceOf(Buffer).with.lengthOf(1);
-        should(data.payload_raw[0]).equal(1);
-        should(data.counter).equal(5);
-        client.end();
-        done();
-      });
-      client.mqtt.publish(client.appId + '/devices/a-device/up', JSON.stringify({
+      var data = {
         "port": 1,
         "counter": 5,
         "payload_raw": "AQ==",
@@ -160,47 +162,45 @@ describe('Client', function() {
             "rf_chain": 1
           }]
         }
-      }));
+      };
+      var callback = sinon.spy();
+      client.on('message', callback);
+      client.mqtt.publish(client.appId + '/devices/a-device/up', JSON.stringify(data));
+      setTimeout(function () {
+        data.payload_raw = new Buffer(data.payload_raw, 'base64');
+        callback.should.be.calledOnce().calledWith('a-device', data);
+        done();
+      }, WAIT_FOR);
     });
 
-    it('should emit event for specific device', function(done) {
+    it('should emit event for specific device', function (done) {
       var client = createClient();
-      client.on('message', 'a-device', function(deviceId, data) {
-        should(arguments.length).equal(2);
-        should(deviceId).equal('a-device');
-        should(data).be.an.Object();
-        should(data.payload_raw).be.an.instanceOf(Buffer).with.lengthOf(1);
-        should(data.payload_raw[0]).equal(1);
-        client.end();
-        done();
-      });
-      client.mqtt.publish(client.appId + '/devices/a-device/up', JSON.stringify({
+      var data = {
         payload_raw: 'AQ=='
-      }));
+      };
+      var callback = sinon.spy();
+      client.on('message', 'a-device', callback);
+      client.mqtt.publish(client.appId + '/devices/a-device/up', JSON.stringify(data));
+      setTimeout(function () {
+        data.payload_raw = new Buffer(data.payload_raw, 'base64');
+        callback.should.be.calledOnce().calledWith('a-device', data);
+        done();
+      }, WAIT_FOR);
     });
 
-    it('should emit event for specific device and field', function(done) {
+    it('should emit event for specific (device and) field', function (done) {
       var client = createClient();
-      client.on('message', 'a-device', 'led', function(deviceId, data) {
-        should(arguments.length).equal(2);
-        should(deviceId).equal('a-device');
-        should(data).be.true();
-        client.end();
+      var data = true;
+      var callbackF = sinon.spy();
+      var callbackDF = sinon.spy();
+      client.on('message', null, 'led', callbackF);
+      client.on('message', 'a-device', 'led', callbackDF);
+      client.mqtt.publish(client.appId + '/devices/a-device/up/led', JSON.stringify(data));
+      setTimeout(function () {
+        callbackF.should.be.calledOnce().calledWith('a-device', data);
+        callbackDF.should.be.calledOnce().calledWith('a-device', data);
         done();
-      });
-      client.mqtt.publish(client.appId + '/devices/a-device/up/led', JSON.stringify(true));
-    });
-
-    it('should emit event for specific field', function(done) {
-      var client = createClient();
-      client.on('message', null, 'led', function(deviceId, data) {
-        should(arguments.length).equal(2);
-        should(deviceId).equal('a-device');
-        should(data).be.false();
-        client.end();
-        done();
-      });
-      client.mqtt.publish(client.appId + '/devices/a-device/up/led', JSON.stringify(false));
+      }, WAIT_FOR);
     });
 
   });
@@ -209,5 +209,10 @@ describe('Client', function() {
 function createClient() {
   client = new ttn.Client(BROKER); // don't pass appId and appAccessKey to the test broker
   client.appId = Math.floor(Math.random() * 16777215).toString(16); // but set random appId afterwards
+  if (BYPASS) { // bypass MQTT server for faster and more reliable tests
+    client.mqtt.publish = function(topic, message) {
+      client.mqtt.emit('message', topic, message);
+    };
+  }
   return client;
 }
