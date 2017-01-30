316695d82b7d5bdef26b90b7f3c1a05a21cb0e2b
Romeo Van Snick <romeovs@gmail.com>
2017-01-30 17:09:46 +0100

    Update example

--- example.js
+++ example.js
@@ -1,75 +1,64 @@
-const ttn = require('.');
+import ttn from "."
 
 // This package provides a sample to test the sdk.
 // Replace the app-id and the dev-id and provide your credentials to test it
 
 var options = {
-  region: 'Your region (eu or us)',
-  key: 'Your app Access Key',
-  token: 'Your token'
+  region: "eu" // Your region (eu or us),
+  key: "", // Your app access key
 }
 
-var client = new ttn.manager.HTTP(options);
+var client = new ttn.manager.HTTP(options)
 
-client.getDevice({
-  app_id: 'app-id',
-  dev_id: 'dev-id'
-}, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+client.getDevice("app-id", "dev-id"
+  .then(function (device) {
+    console.log(device)
+  })
+  .catch(function (error) {
+    console.error("Got error", error)
+  })
 
-client.getDevicesForApplication({
-  app_id: 'app-id',
-}, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+client.getDevicesForApplication("app-id")
+  .then(function (devices) {
+    console.log(devices)
+  })
+  .catch(function (error) {
+    console.error("Got error", error)
+  })
 
-client.registerApplication({}, {
-  app_id: 'app-id'
-}, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+client.registerApplication("app-id", {})
+  .then(function () {
+    console.log("Registered!")
+  })
+  .catch(function (error) {
+    console.error("Got error", error)
+  })
 
-client.setApplication({
-  app_id: 'app-id',
-  converter: 'function Converter(decoded, port) { return {name: John, lastname: Snow}; }',
-  decoder: 'function Decoder(bytes, port) { return {test: value}; }',
-  validator: 'function Validator(conve, port) { return false; }',
-  encoder: 'function Encoder(object, port) { return []; }',
-}, {
-  app_id: 'app-id',
-  converter: 'function Converter(decoded, port) { return {name: John, lastname: Snow}; }',
-  decoder: 'function Decoder(bytes, port) { return {test: value}; }',
-  validator: 'function Validator(conve, port) { return false; }',
-  encoder: 'function Encoder(object, port) { return []; }',
-}, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+client.setApplication("app-id", {
+  converter: "function Converter(decoded, port) { return {name: John, lastname: Snow}; }",
+  decoder: "function Decoder(bytes, port) { return {test: value}; }",
+  validator: "function Validator(conve, port) { return false; }",
+  encoder: "function Encoder(object, port) { return []; }",
+})
+  .then(function () {
+    console.log("Set!")
+  })
+  .catch(function (error) {
+    console.error("Got error", error)
+  })
 
-client.getApplication({
-  app_id: 'app-id'
-}, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+client.getApplication("app-id")
+  .then(function (app) {
+    console.log(app);
+  })
+  .catch(function (error) {
+    console.error("Got error", error)
+  })
 
-client.setDevice({
-  app_id: 'app-id',
-  dev_id: 'dev-id'
-},{
-  app_id: 'app-id',
-  dev_id: 'dev-id'
-}, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-});
+client.setDevice("app-id", "dev-id", {})
+  .then(function () {
+    console.log("Set!")
+  })
+  .catch(function (error) {
+    console.error("Got error", error)
+  })
