3edbf85615195bac4c92e70223d0e44a1a12f17c
Romeo Van Snick <romeovs@gmail.com>
2017-01-30 16:43:40 +0100

    Make HTTP manager an explicit class

--- src/manager/http.js
+++ src/manager/http.js
@@ -1,73 +1,120 @@
-'use strict';
-
-const request = require('request');
+require('isomorphic-fetch');
+const httpError = require('http-errors');
 const utils = require('../lib/utils');
+const regions = require('../regions');
+const URL = require('url');
 
-let defaults = {
+const defaults = {
   method: 'GET',
   json: true
-};
+}
 
-let methods = {
-  RegisterApplication: {
-    method: 'POST',
-    uri: '/applications'
-  },
-  GetApplication: {
-    uri: '/applications/{app_id}'
-  },
-  SetApplication: {
-    method: 'POST',
-    uri: '/applications/{app_id}'
-  },
-  DeleteApplication: {
-    method: 'DELETE',
-    uri: '/applications/{app_id}'
-  },
-  GetDevice: {
-    uri: '/applications/{app_id}/devices/{dev_id}'
-  },
-  RegisterDevice: {
-    method: 'POST',
-    uri: '/applications/{app_id}/devices'
-  },
-  SetDevice: {
-    method: 'POST',
-    uri: '/applications/{app_id}/devices/{dev_id}'
-  },
-  DeleteDevice: {
-    method: 'DELETE',
-    uri: '/applications/{app_id}/devices/{dev_id}'
-  },
-  GetDevicesForApplication: {
-    uri: '/applications/{app_id}/devices'
+const checkStatus = function (response) {
+  if (response.status < 200 || response.status >= 300) {
+    throw httpError(response.status, reponse.statusText)
   }
-};
+  return response
+}
+
+const normalize = function (url = "") {
+  let withProtocol = url.indexOf("://") === -1 ? "https://".concat(url) : url
+
+  const u = URL.parse(withProtocol)
+  if (!u.port) {
+    u.host = u.host.concat(":8084")
+  }
+
+  return URL.format(u)
+}
+
+class HTTP {
+  constructor (options = {}) {
+    if (options.key && options.token) {
+      return this.error('Both key and token given')
+    }
+
+    if (!options.key && !options.token) {
+      return this._error('No key or token given')
+    }
+
+    this.key = options.key
+    this.token = options.token
+
+    if (options.region && options.base) {
+      return this._error('Both region and base are given, only one is allowed')
+    }
+
+    if (!options.region && !options.base) {
+      return this._error('No region or base are given')
+    }
+
+    if (options.region) {
+      this.region = regions.validate(options.region);
+    }
+
+    this.base = options.base || normalize(this.region)
+
+    this.headers = options.headers || {}
+  }
+
+  // throw a prefixed error
+  _error (msg) {
+    throw new Error(`HTTP: ${msg || 'unknown error'}`)
+  }
+
+  // perform a request
+  _request (method = 'GET', uri, body) {
+    const url = URL.resolve(this.base, uri)
+
+    return fetch(url, {
+      method,
+      headers: {
+        ...(this.headers || {}),
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: this.key ? `Key ${this.key}` : `Bearer ${this.token}`,
+      },
+      body: body && JSON.stringify(body),
+    })
+    .then(checkStatus)
+    .then(r => r.json())
+  }
+
+  getApplication (appID) {
+    return this._request('GET', `/applications/${appID}`)
+  }
+
+  registerApplication (app) {
+    return this._request('POST', '/applications', app)
+  }
+
+  setApplication (appID, app) {
+    return this._request('POST', `/applications/${appID}`, app)
+  }
+
+  deleteApplication (appID) {
+    return this._request('DELETE', `/applications/${appID}`)
+  }
+
+  getDevicesForApplication (appID) {
+    return this._request('GET', `/applications/${appID}/devices`)
+  }
+
+  getDevice (appID, devID) {
+    return this._request('GET', `/applications/${appID}/devices/${devID}`)
+  }
+
+  registerDevice (appID, device) {
+    return this.SetDevice(appID, device.devID, device)
+  }
+
+  setDevice (appID, devID, device) {
+    return this._request('POST', `/applications/${appID}/devices`, device)
+  }
+
+  deleteDevice (appID, devID) {
+    return this._request('DELETE', `/applications/${appID}/devices`)
+  }
+}
 
 module.exports = HTTP;
-
-function HTTP(instanceOptions = {}) {
-
-  Object.getOwnPropertyNames(methods).forEach((methodName) => {
-
-    this[utils.serviceToFunction(methodName)] = function (functionOptions, body, callback) {
-
-      if (arguments.length === 1) {
-        callback = functionOptions;
-        functionOptions = {};
-      } else if (arguments.length === 2) {
-        callback = body;
-      }
-
-      let options = Object.assign({}, defaults, instanceOptions, methods[methodName], functionOptions);
-
-      utils.expandRegion(options);
-      utils.expandAuth(options);
-      utils.expandUri(options);
-
-      options.body = body || functionOptions;
-
-      return request(options, callback);
-    }
-  });
-}
