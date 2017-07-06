a8597b386cf1c012593e5a6196387c2a7e6b0d9b
Romeo Van Snick <romeovs@gmail.com>
2017-07-06 21:15:28 +0200

    Also typecheck examples

--- examples/discovery.js
+++ examples/discovery.js
@@ -1,6 +1,8 @@
 // Copyright © 2017 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENCE file.
 
+// @flow
+
 import fs from "fs"
 import { Discovery, services } from "../src/discovery"
 
