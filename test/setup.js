dbc055e8cc9046493402e907eb1015cf5ec55645
Romeo Van Snick <romeovs@gmail.com>
2017-07-12 11:11:16 +0200

    Add stubs and setup for tests

---
+++ test/setup.js
@@ -0,0 +1,34 @@
+// Copyright © 2017 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENCE file.
+
+// @flow
+
+import grpc from "grpc"
+
+import wrap from "../src/utils/wrap"
+import proto from "../src/proto/ttn/api/handler/handler_pb"
+import handler from "../src/proto/ttn/api/handler/handler_grpc_pb"
+
+import * as stubs from "./stubs"
+
+const netAddress = "localhost:1904"
+
+/**
+ * Setup function that prepares the environment with the required
+ * application and devices for testing.
+ */
+export default async function () {
+  const credentials = grpc.credentials.createSsl(stubs.handler.certificate)
+  const client = new handler.ApplicationManagerClient(netAddress, credentials)
+
+  // register the test app
+  const req = new proto.ApplicationIdentifier()
+  req.setAppId(stubs.app.id)
+
+  const meta = new grpc.Metadata()
+  meta.add("token", stubs.app.accessToken)
+
+  // reregister app
+  await wrap(client, client.deleteApplication, req, meta).catch(() => null)
+  await wrap(client, client.registerApplication, req, meta)
+}
---
+++ test/stubs.js
@@ -0,0 +1,68 @@
+// Copyright © 2017 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENCE file.
+
+// @flow
+
+import fs from "fs"
+import path from "path"
+import jwt from "jsonwebtoken"
+
+const key = fs.readFileSync(path.resolve(__dirname, ".env/discovery/server.key"))
+const options = {
+  issuer: "local",
+  expiresIn: "7d",
+  algorithm: "ES256",
+}
+
+const claims = {
+  scope: [ "apps" ],
+}
+
+const appClaims = {
+  scope: [ "apps:test" ],
+  apps: {
+    test: [
+      "settings",
+      "devices",
+      "delete",
+    ],
+  },
+}
+
+/**
+ * A token that can register ApplicationSettings
+ */
+export const token = jwt.sign(claims, key, options)
+
+/**
+ * The app used for testing
+ */
+export const app = {
+  id: "test",
+  accessToken: jwt.sign(appClaims, key, options),
+}
+
+/**
+ * Settings for the discovery server
+ */
+export const discovery = {
+  address: "localhost:1900",
+  certificate: fs.readFileSync(path.resolve(__dirname, ".env/discovery/server.cert")),
+}
+
+/**
+ * Settings for the handler
+ */
+export const handler = {
+  id: "dev",
+  serviceName: "handler",
+  description: "",
+  url: "",
+  pb_public: false,
+  netAddress: "handler:1904",
+  publicKey: fs.readFileSync(path.resolve(__dirname, ".env/handler/server.pub")),
+  certificate: fs.readFileSync(path.resolve(__dirname, ".env/handler/server.cert")),
+  apiAddress: "http://handler:8084",
+  mqttAddress: "handler:1883",
+  amqpAddress: "handler:5672",
+}
