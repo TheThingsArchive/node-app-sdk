6c2f111dbdfa3fcb935bb087de31e931b9f8a02c
Romeo Van Snick <romeovs@gmail.com>
2017-07-13 13:40:37 +0200

    Add stubs for data api

--- src/data/data.js
+++ src/data/data.js
@@ -3,7 +3,12 @@
 
 // @flow
 
-export type EventName = "connect" | "error" | "message" | "device" | "activation"
+import EventEmitter from "events"
+import mqtt from "mqtt"
+
+type Connack = {
+  sessionPresent : boolean,
+}
 
 export class DataClient {
   /** @private */
@@ -15,9 +20,65 @@ export class DataClient {
   /** @private */
   appAccessKey : string
 
+  /** @private */
+  emitter : EventEmitter
+
+  /** @private */
+  mqtt : any
+
+  /**
+   * Creates a new DataClient and opens the MQTT connection
+   */
   constructor (appID : string, appAccessKey : string, mqttAddress : string) : void {
     this.mqttAddress = mqttAddress
     this.appID = appID
     this.appAccessKey = appAccessKey
+
+    this.emitter = new EventEmitter()
+    this.mqtt = mqtt.connect({
+      host: mqttAddress,
+      username: appID,
+      password: appAccessKey,
+    })
+
+    this.mqtt.on("error", this.onError)
+    this.mqtt.on("connect", this.onConnect)
+    this.mqtt.on("message", this.onMessage)
+  }
+
+  /**
+   * Close the mqtt connection
+   */
+  close (...args : any) {
+    return this.mqtt.end(...args)
+  }
+
+  /**
+   * Starts listening to events
+   */
+  on () {
+    // TODO
+  }
+
+  /**
+   * Stop listening to events
+   */
+  off () {
+    // TODO
+  }
+
+  /** @private */
+  onError (err : any) : void {
+    this.emitter.emit("error", err)
+  }
+
+  /** @private */
+  onConnect (ack : Connack) : void {
+    this.emitter.emit("connect", ack)
+  }
+
+  /** @private */
+  onMessage (message : any) : void {
+    // TODO
   }
 }
