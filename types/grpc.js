0533cd7a4725d567584664c970290aa8108cfc1d
Norwin Roosen <github@nroo.de>
2017-11-16 13:13:54 +0100

    rm unused types/ directory

--- types/grpc.js
+++
@@ -1,7 +0,0 @@
-// Copyright © 2017 The Things Network
-// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
-
-
-declare module "grpc" {
-  declare module.exports : any
-}
